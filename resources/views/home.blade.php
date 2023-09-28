fdd32032b1afa434792bbef01674faefcb2409dd
USER_NAME <USER_EMAIL>
2023-09-28 16:53:50 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.main')
+@push('title')
+<title>Home</title>
+@endpush
+
+@section('main-section')
+<h1 class="text-center">
+    Home Page
+</h1>
+
+@endsection
+
+   
+
+   
+
+
+ 
+
+
+
+
+    
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\democontroller;
+use App\Http\Controllers\photocongtroller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[democontroller::class,'index']);
+Route::get('/about','App\Http\Controllers\democontroller@about');
+Route::resource('photo',photocongtroller::class);
