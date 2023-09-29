9706ab5d2140129c9a9191fcc4fcae6d418017a5
tusharpatekar45 <tusharpatekar45@gmail.com>
2023-09-29 16:39:07 +0530

    Roles And Permission

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *@return void
+     */
+    public function run()
+    {
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        $testPermission = [
+            ['name' => 'dashboard'],
+        ];
+        foreach ($testPermission as $item) {
+            Permission::firstOrCreate($item);
+        }
+        $adminRole->syncPermissions(Permission::all());
+        $user = User::where('email', 'admin@gmail.com')->first();
+        $user->assignRole('admin');
+    }
+
+}
