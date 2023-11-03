57e20020332006e91e09cb0194a01895e9984d83
Jalindranath <jalindranathkangane38@gmail.com>
2023-11-04 00:22:54 +0530

    Teacher Functionality

--- database/migrations/2023_11_03_164254_create_faculties_table.php
+++ database/migrations/2023_11_03_164254_create_faculties_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('faculty', function (Blueprint $table) {
+        Schema::create('faculties', function (Blueprint $table) {
             $table->id();
             $table->string('fname');
             $table->string('fdepart')->nullable();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('faculty');
+        Schema::dropIfExists('faculties');
     }
 };
