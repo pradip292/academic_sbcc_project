e67153dcf42cb0b4b2a620e97ad29359b6afeabf
Jalindranath <jalindranathkangane38@gmail.com>
2023-11-04 09:19:42 +0530

    Teacher search

--- resources/views/faculty/view_faculty.blade.php
+++ resources/views/faculty/view_faculty.blade.php
@@ -1,9 +1,15 @@
 @extends('layout.base')
+
 @section('content')
     <div class="card">
         <div class="card-body">
             <br>
             <h5 class="card-title">Faculties Details</h5>
+
+            <div class="form-group">
+                <input type="text" class="form-control" id="search" placeholder="Search Faculty">
+            </div>
+
             <table class="table">
                 <thead>
                     <tr>
@@ -20,7 +26,7 @@
                             <td>{{ $serialNumber }}</td>
                             <td>{{ $item->fname }}</td>
                             <td>{{ $item->fdepart }}</td>
-                            <td>{{ $item->fmail }}</td>    
+                            <td>{{ $item->fmail }}</td>
                         </tr>
                         @php $serialNumber++ @endphp
                     @endforeach
@@ -28,4 +34,25 @@
             </table>
         </div>
     </div>
+
+    <script>
+        // Function to handle the search input
+        document.getElementById('search').addEventListener('input', function () {
+            var searchValue = this.value.toLowerCase();
+            var rows = document.querySelectorAll('tbody tr');
+
+            rows.forEach(function (row) {
+                var name = row.querySelector('td:nth-child(2)').textContent.toLowerCase();
+                var department = row.querySelector('td:nth-child(3)').textContent.toLowerCase();
+                var email = row.querySelector('td:nth-child(4)').textContent.toLowerCase();
+
+                // Check if the search value is found in any of the columns
+                if (name.includes(searchValue) || department.includes(searchValue) || email.includes(searchValue)) {
+                    row.style.display = 'table-row';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        });
+    </script>
 @endsection
