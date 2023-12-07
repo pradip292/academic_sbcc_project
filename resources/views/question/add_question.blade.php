3f6d9ead85c1e49c8043ab87f3122856d1fc6b64
pradip292 <pradipwarkhade74@gmail.com>
2023-12-07 19:14:48 +0530

    question error solved 50per

--- resources/views/question/add_question.blade.php
+++ resources/views/question/add_question.blade.php
@@ -4,7 +4,7 @@
         <div class="col">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Upload Student Feedback 1 Theory Question's </h5>
+                    <h5 class="card-title">Upload Student Feedback Theory Question's </h5>
                     <input type="hidden" value="1" name="term" />
                     <form action="{{ route('upload-question-theory') }}" method="post" enctype="multipart/form-data">
                         @csrf
@@ -21,50 +21,10 @@
                         </div>
 
                     </form>
-                    <h5 class="card-title">Upload Student Feedback 1 Practical Question's </h5>
+                    <h5 class="card-title">Upload Student Feedback Practical Question's </h5>
                     <form action="{{ route('upload-question-practical') }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <input type="hidden" value="1" name="term" />
-                        <div class="row mb-3">
-                            <label for="inputNumber" class="col-sm-2 col-form-label">File Upload</label>
-                            <div class="col-sm-10">
-                                <input class="form-control" type="file" name="file">
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-sm-10">
-                                <button type="submit" class="btn btn-primary">Upload</button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Upload Student Feedback 2 Theory Question's</h5>
-                    <form action="{{ route('upload-question-theory') }}" method="post" enctype="multipart/form-data">
-                        @csrf
-                        <input type="hidden" value="2" name="term" />
-                        <div class="row mb-3">
-                            <label for="inputNumber" class="col-sm-2 col-form-label">File Upload</label>
-                            <div class="col-sm-10">
-                                <input class="form-control" type="file" name="file">
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-sm-10">
-                                <button type="submit" class="btn btn-primary">Upload</button>
-                            </div>
-                        </div>
-
-                    </form>
-                    <h5 class="card-title">Upload Student Feedback 2 Practical Question's</h5>
-                    <form action="{{ route('upload-question-practical') }}" method="post" enctype="multipart/form-data">
-                        @csrf
-                        <input type="hidden" value="2" name="term" />
                         <div class="row mb-3">
                             <label for="inputNumber" class="col-sm-2 col-form-label">File Upload</label>
                             <div class="col-sm-10">
